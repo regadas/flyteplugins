0311a4396ed0d754ac12cf9d075c201058136794
Filipe Regadas <filiperegadas@gmail.com>
2020-11-27 11:46:12 +0000

    Add initial flink plugin

--- go.mod
+++ go.mod
@@ -10,30 +10,28 @@ require (
 	github.com/coocood/freecache v1.1.0
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-test/deep v1.0.5
-	github.com/golang/protobuf v1.3.5
+	github.com/golang/protobuf v1.4.3
 	github.com/hashicorp/golang-lru v0.5.4
 	github.com/kubeflow/pytorch-operator v0.6.0
 	github.com/kubeflow/tf-operator v0.5.3
 	github.com/lyft/flyteidl v0.18.9
 	github.com/lyft/flytestdlib v0.3.9
 	github.com/magiconair/properties v1.8.1
-	github.com/mitchellh/mapstructure v1.1.2
+	github.com/mitchellh/mapstructure v1.3.3
 	github.com/pkg/errors v0.9.1
-	github.com/prometheus/client_golang v1.5.0
-	github.com/spf13/cobra v0.0.6 // indirect
+	github.com/prometheus/client_golang v1.8.0
+
+	// github.com/lyft/flinkk8soperator v0.5.0
+	github.com/regadas/flink-on-k8s-operator v0.0.0-20201126113113-ee4a54861ce2
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
-	google.golang.org/api v0.16.0 // indirect
-	google.golang.org/genproto v0.0.0-20200205142000-a86caf926a67 // indirect
-	google.golang.org/grpc v1.28.0
-	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+	google.golang.org/grpc v1.33.2
 	gotest.tools v2.2.0+incompatible
-	k8s.io/api v0.17.3
-	k8s.io/apimachinery v0.17.3
+	k8s.io/api v0.19.4
+	k8s.io/apimachinery v0.18.3
 	k8s.io/client-go v11.0.1-0.20190918222721-c0e3722d5cf0+incompatible
-	k8s.io/utils v0.0.0-20200124190032-861946025e34 // indirect
-	sigs.k8s.io/controller-runtime v0.5.1
+	sigs.k8s.io/controller-runtime v0.6.0
 )
 
 // Pin the version of client-go to something that's compatible with katrogan's fork of api and apimachinery
@@ -44,7 +42,30 @@ require (
 replace (
 	github.com/GoogleCloudPlatform/spark-on-k8s-operator => github.com/lyft/spark-on-k8s-operator v0.1.4-0.20201027003055-c76b67e3b6d0
 	github.com/googleapis/gnostic => github.com/googleapis/gnostic v0.3.1
+	//FIXME(regadas):
+	github.com/lyft/flyteidl => /Users/regadas/projects/flyteidl
 	k8s.io/api => github.com/lyft/api v0.0.0-20191031200350-b49a72c274e0
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.18.3
 	k8s.io/apimachinery => github.com/lyft/apimachinery v0.0.0-20191031200210-047e3ea32d7f
+	k8s.io/apiserver => k8s.io/apiserver v0.18.3
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.18.3
 	k8s.io/client-go => k8s.io/client-go v0.0.0-20191016111102-bec269661e48
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.18.3
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.18.3
+	k8s.io/code-generator => k8s.io/code-generator v0.16.10-beta.0
+	k8s.io/component-base => k8s.io/component-base v0.18.3
+	k8s.io/cri-api => k8s.io/cri-api v0.18.3
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.18.3
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.18.3
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.18.3
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.18.3
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.18.3
+	k8s.io/kubectl => k8s.io/kubectl v0.18.3
+	k8s.io/kubelet => k8s.io/kubelet v0.18.3
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.18.3
+	k8s.io/metrics => k8s.io/metrics v0.18.3
+	k8s.io/node-api => k8s.io/node-api v0.18.3
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.18.3
+	k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.18.3
+	k8s.io/sample-controller => k8s.io/sample-controller v0.18.3
 )
---
+++ go/tasks/plugins/k8s/flink/flink.go
@@ -0,0 +1,203 @@
+package flink
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/flytek8s"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/flytek8s/config"
+
+	"github.com/lyft/flyteplugins/go/tasks/errors"
+	pluginsCore "github.com/lyft/flyteplugins/go/tasks/pluginmachinery/core"
+
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/k8s"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+
+	flinkOp "github.com/regadas/flink-on-k8s-operator/api/v1beta1"
+
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/plugins"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	pluginsConfig "github.com/lyft/flyteplugins/go/tasks/config"
+)
+
+const KindFlinkCluster = "FlinkCluster"
+
+var flinkTaskType = "flink"
+
+// Config ... Flink-specific configs
+type Config struct {
+	DefaultFlinkConfig map[string]string `json:"flink-config-default" pflag:",Key value pairs of default flink configuration that should be applied to every FlinkJob"`
+}
+
+var (
+	flinkConfigSection = pluginsConfig.MustRegisterSubSection("flink", &Config{})
+)
+
+func GetFlinkConfig() *Config {
+	return flinkConfigSection.GetConfig().(*Config)
+}
+
+// This method should be used for unit testing only
+func setFlinkConfig(cfg *Config) error {
+	return flinkConfigSection.SetConfig(cfg)
+}
+
+type flinkResourceHandler struct {
+}
+
+func (flinkResourceHandler) GetProperties() pluginsCore.PluginProperties {
+	return pluginsCore.PluginProperties{}
+}
+
+// Creates a new Job that will execute the main container as well as any generated types the result from the execution.
+func (flinkResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsCore.TaskExecutionContext) (k8s.Resource, error) {
+	taskTemplate, err := taskCtx.TaskReader().Read(ctx)
+	if err != nil {
+		return nil, errors.Errorf(errors.BadTaskSpecification, "unable to fetch task specification [%v]", err.Error())
+	} else if taskTemplate == nil {
+		return nil, errors.Errorf(errors.BadTaskSpecification, "nil task specification")
+	}
+
+	flinkJob := plugins.FlinkJob{}
+	err = utils.UnmarshalStruct(taskTemplate.GetCustom(), &flinkJob)
+	if err != nil {
+		return nil, errors.Wrapf(errors.BadTaskSpecification, err, "invalid TaskSpecification [%v], failed to unmarshal", taskTemplate.GetCustom())
+	}
+
+	container := taskTemplate.GetContainer()
+	envVars := flytek8s.DecorateEnvVars(ctx, flytek8s.ToK8sEnvVar(container.GetEnv()), taskCtx.TaskExecutionMetadata().GetTaskExecutionID())
+
+	flinkEnvVars := make(map[string]string)
+	for _, envVar := range envVars {
+		flinkEnvVars[envVar.Name] = envVar.Value
+	}
+	flinkEnvVars["FLYTE_MAX_ATTEMPTS"] = strconv.Itoa(int(taskCtx.TaskExecutionMetadata().GetMaxAttempts()))
+
+	jobManagerReplicas := int32(1)
+	JobManagerUiPort := int32(8081)
+
+	jobManager := flinkOp.JobManagerSpec{
+		Replicas: &jobManagerReplicas,
+		Ports: flinkOp.JobManagerPorts{
+			UI: &JobManagerUiPort,
+		},
+	}
+
+	taskManagerReplicas := int32(1)
+	taskManager := flinkOp.TaskManagerSpec{
+		Replicas: taskManagerReplicas,
+	}
+
+	// Start with default config values.
+	flinkProperties := make(map[string]string)
+	for k, v := range GetFlinkConfig().DefaultFlinkConfig {
+		flinkProperties[k] = v
+	}
+
+	for k, v := range flinkJob.GetFlinkProperties() {
+		flinkProperties[k] = v
+	}
+
+	fc := &flinkOp.FlinkCluster{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       KindFlinkCluster,
+			APIVersion: flinkOp.GroupVersion.String(),
+		},
+		Spec: flinkOp.FlinkClusterSpec{
+			Image: flinkOp.ImageSpec{
+				Name:       "flink:1.10.1-scala_2.12",
+				PullPolicy: corev1.PullAlways,
+			},
+			JobManager:  jobManager,
+			TaskManager: taskManager,
+			Job: &flinkOp.JobSpec{
+				JarFile:   flinkJob.JarFile,
+				ClassName: &flinkJob.MainClass,
+			},
+			FlinkProperties: flinkProperties,
+		},
+	}
+
+	// Add Tolerations/NodeSelector to only Executor pods.
+	if taskCtx.TaskExecutionMetadata().IsInterruptible() {
+		fc.Spec.TaskManager.Tolerations = config.GetK8sPluginConfig().InterruptibleTolerations
+		fc.Spec.TaskManager.NodeSelector = config.GetK8sPluginConfig().InterruptibleNodeSelector
+	}
+
+	return fc, nil
+}
+
+func (flinkResourceHandler) BuildIdentityResource(ctx context.Context, taskCtx pluginsCore.TaskExecutionMetadata) (k8s.Resource, error) {
+	return &flinkOp.FlinkCluster{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       KindFlinkCluster,
+			APIVersion: flinkOp.GroupVersion.String(),
+		},
+	}, nil
+}
+
+func getEventInfoForFlink(fc *flinkOp.FlinkCluster) (*pluginsCore.TaskInfo, error) {
+	var taskLogs []*core.TaskLog
+	customInfoMap := make(map[string]string)
+
+	customInfo, err := utils.MarshalObjToStruct(customInfoMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pluginsCore.TaskInfo{
+		Logs:       taskLogs,
+		CustomInfo: customInfo,
+	}, nil
+}
+
+func (flinkResourceHandler) GetTaskPhase(ctx context.Context, pluginContext k8s.PluginContext, resource k8s.Resource) (pluginsCore.PhaseInfo, error) {
+
+	app := resource.(*flinkOp.FlinkCluster)
+	info, err := getEventInfoForFlink(app)
+	if err != nil {
+		return pluginsCore.PhaseInfoUndefined, err
+	}
+
+	occurredAt := time.Now()
+	// TODO(regadas): we should probably cover cluster state
+	switch app.Status.Components.Job.State {
+	case flinkOp.JobStatePending, flinkOp.JobStateUpdating:
+		return pluginsCore.PhaseInfoQueued(occurredAt, pluginsCore.DefaultPhaseVersion, "job queued"), nil
+	case flinkOp.JobStateRunning:
+		return pluginsCore.PhaseInfoInitializing(occurredAt, pluginsCore.DefaultPhaseVersion, "job submitted", info), nil
+	case flinkOp.JobStateFailed:
+		reason := fmt.Sprintln("Flink Job  Submission Failed with Error")
+		return pluginsCore.PhaseInfoRetryableFailure(errors.DownstreamSystemError, reason, info), nil
+	case flinkOp.JobStateCancelled:
+		reason := fmt.Sprintf("Flink Job cancelled")
+		return pluginsCore.PhaseInfoRetryableFailure(errors.DownstreamSystemError, reason, info), nil
+	case flinkOp.JobStateSucceeded:
+		return pluginsCore.PhaseInfoSuccess(info), nil
+	}
+
+	return pluginsCore.PhaseInfoRunning(pluginsCore.DefaultPhaseVersion, info), nil
+}
+
+func init() {
+	if err := flinkOp.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+
+	pluginmachinery.PluginRegistry().RegisterK8sPlugin(
+		k8s.PluginEntry{
+			ID:                  flinkTaskType,
+			RegisteredTaskTypes: []pluginsCore.TaskType{flinkTaskType},
+			ResourceToWatch:     &flinkOp.FlinkCluster{},
+			Plugin:              flinkResourceHandler{},
+			IsDefault:           false,
+			DefaultForTaskTypes: []pluginsCore.TaskType{flinkTaskType},
+		})
+}
